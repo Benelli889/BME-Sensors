696e7ea5197142e5e177f5b0bca4030fbaa90e78
Arndt <arndt.steffens@mail.de>
2021-11-30 21:58:23 +0100

    MQTT new

--- air-quality.py
+++ air-quality.py
@@ -2,6 +2,9 @@
 
 import bme680
 import time
+from threading import Thread
+import context  # Ensures paho is in PYTHONPATH
+# https://github.com/eclipse/paho.mqtt.python
 import paho.mqtt.client as mqtt
 from LoggerClass import LoggerClass
 
@@ -14,8 +17,6 @@ print("""indoor-air-quality.py - Estimates indoor air quality.
 Runs the sensor for a burn-in period, then uses a
 combination of relative humidity and gas resistance
 to estimate indoor air quality as a percentage.
-
-Press Ctrl+C to exit!
 
 """)
 
@@ -38,52 +39,147 @@ sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(150)
 sensor.select_gas_heater_profile(0)
 
-# start_time and curr_time ensure that the
-# burn_in_time (in seconds) is kept track of.
-
-start_time = time.time()
-curr_time = time.time()
+# Set the humidity baseline to 40%, an optimal indoor humidity.
+hum_baseline = 40.0
+# This sets the balance between humidity and gas reading in the
+# calculation of air_quality_score (25:75, humidity:gas)
+hum_weighting = 0.25
+gas_baseline = 0
 #burn_in_time = 300
-burn_in_time = 5
+burn_in_time = 3
 
-
-burn_in_data = []
 
 BROKER_ADDRESS = '192.168.178.105'
 PORT = 1883
-QOS = 0
+QOS = 1
 DATA = "{TEST_DATA}"
 TOPIC = 'BME680/temp'
 
-client = mqtt.Client()
-client.connect(BROKER_ADDRESS, PORT)
-client.subscribe(TOPIC, 1)
 
-print("Connected to MQTT Broker: " + BROKER_ADDRESS)
-Log.Msg("Connected to MQTT Broker: " + BROKER_ADDRESS)
+class MqttHandler(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.client = mqtt.Client()
+        self.daemon = True
+        self.start()
+
+        self.client.on_connect = self.on_connect
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_message = self.on_message
+        self.client.on_publish = self.on_publish
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_connected_new = self.on_connected_new
+
+        self.client.connect(BROKER_ADDRESS, PORT)
+        self.client.subscribe(TOPIC, 1)
+
+        print("Connected to MQTT Broker: " + BROKER_ADDRESS)
+        Log.Msg("Connected to MQTT Broker: " + BROKER_ADDRESS)
+
+        self.is_connected_flag = True
+        self.ON_MESSAGE_GLOBAL = 0xFF
+        self.DATA = 0
+
+    def run(self):
+        Log.Msg(" -- RUN --")
+        while True:
+            self.client.loop()
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("connected")
+        print("userdata: {}, flags: {}, rc: {}".format(userdata, flags, rc))
+        Log.Msg("userdata: {}, flags: {}, rc: {}".format(userdata, flags, rc))
+
+        self.is_connected_flag = True
+
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        print("subscribed")
+        Log.Msg("subscribed")
+
+    def on_publish(self, client, userdata, mid):
+        print("message published")
+        Log.Msg("message published")
+
+    def on_message(self, client, userdata, message):
+        print("message printed to topic")
+        Log.Msg("message printed to topic")
+        print("userdata: {}, message.payload: {}, topic: {} qos: {}".format(
+            userdata, str(message.payload), message.topic, message.qos))
+        Log.Msg("userdata: {}, --message.payload: {}, --topic: {} qos: {}".format(
+            userdata, str(message.payload), message.topic, message.qos))
+
+    def on_disconnect(self, client, userdata, rc):
+        print("Client Disconnected")
+        Log.Msg("Client Disconnected")
+
+        if rc != 0:
+            print("Unexpected disconnection.")
+            Log.Msg("Client Disconnected")
+
+            self.is_connected_flag = False
+
+    def on_connected_new(self):
+
+        if (self.is_connected_flag == False):
+
+            try:
+                self.client.connect(BROKER_ADDRESS, PORT)
+
+            except:
+                print("on_connected_new - connect failure")
+                Log.Msg("on_connected_new - connect failure")
+                self.is_connected_flag = False
+
+            try:
+                self.client.subscribe(TOPIC, 1)
+            except:
+                print("on_connected_new - subscribe failure")
+                Log.Msg("on_connected_new - subscribe failure")
+                self.is_connected_flag = False
+
+    def send_data(self, DATA):
+        self.DATA = DATA
+
+        (rc, mid) = self.client.publish(TOPIC, DATA, qos=QOS)
+        Log.Msg('publish_res: {0} : {1}'.format(rc, mid))
+
+        if (rc != 0):  # mqtt.MQTT_ERR_SUCCESS
+            print('publish_res: ', rc, mid)
+            Log.Msg('publish_res: {0} : {1}'.format(rc, mid))
+            self.is_connected_flag = False
 
 
-try:
-    # Collect gas resistance burn-in values, then use the average
-    # of the last 50 values to set the upper limit for calculating
-    # gas_baseline.
-    print('Collecting gas resistance burn-in data for 5 mins\n')
-    while curr_time - start_time < burn_in_time:
-        curr_time = time.time()
-        if sensor.get_sensor_data() and sensor.data.heat_stable:
-            gas = sensor.data.gas_resistance
-            burn_in_data.append(gas)
-            print('Gas: {0} Ohms'.format(gas))
-            time.sleep(1)
+def burn_time():
+
+    # start_time and curr_time ensure that the
+    # burn_in_time (in seconds) is kept track of.
+
+    start_time = time.time()
+    curr_time = time.time()
+
+    burn_in_data = []
+
+    try:
+        # Collect gas resistance burn-in values, then use the average
+        # of the last 50 values to set the upper limit for calculating
+        # gas_baseline.
+        print('Collecting gas resistance burn-in data for 5 mins\n')
+        count = 0
+        while curr_time - start_time < burn_in_time:
+            curr_time = time.time()
+            if sensor.get_sensor_data() and sensor.data.heat_stable:
+                gas = sensor.data.gas_resistance
+                burn_in_data.append(gas)
+                count = count + 1
+                print('{0}: Gas: {1} Ohms'.format(count, gas))
+                Log.Msg('{0}:  Gas: {1} Ohms'.format(count, gas))
+                time.sleep(1)
+
+    except (RuntimeError):
+        pass
 
     gas_baseline = sum(burn_in_data[-50:]) / 50.0
-
-    # Set the humidity baseline to 40%, an optimal indoor humidity.
-    hum_baseline = 40.0
-
-    # This sets the balance between humidity and gas reading in the
-    # calculation of air_quality_score (25:75, humidity:gas)
-    hum_weighting = 0.25
 
     print('Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n'.format(
         gas_baseline,
@@ -93,72 +189,79 @@ try:
         gas_baseline,
         hum_baseline))
 
+
+def main_air_quality(mqtt):
+
+    if sensor.get_sensor_data() and sensor.data.heat_stable:
+        temperatur = sensor.data.temperature
+        pressure = sensor.data.pressure
+
+        gas = sensor.data.gas_resistance
+        gas_offset = gas_baseline - gas
+
+        hum = sensor.data.humidity
+        hum_offset = hum - hum_baseline
+
+        # Calculate hum_score as the distance from the hum_baseline.
+        if hum_offset > 0:
+            hum_score = (100 - hum_baseline - hum_offset)
+            hum_score /= (100 - hum_baseline)
+            hum_score *= (hum_weighting * 100)
+
+        else:
+            hum_score = (hum_baseline + hum_offset)
+            hum_score /= hum_baseline
+            hum_score *= (hum_weighting * 100)
+
+        # Calculate gas_score as the distance from the gas_baseline.
+        if gas_offset > 0:
+            gas_score = (gas / gas_baseline)
+            gas_score *= (100 - (hum_weighting * 100))
+
+        else:
+            gas_score = 100 - (hum_weighting * 100)
+
+        # Calculate air_quality_score.
+        air_quality_score = hum_score + gas_score
+
+        # print('Temp: {0:.2f} C - {1:.2f} hPa - Humidity: {2:.3f} %RH - Gas: {3:.2f} Ohms - Air quality: {4:.2f}'.format(
+        #    temperatur,
+        #    pressure,
+        #    hum,
+        #    gas,
+        #    air_quality_score))
+
+        print('Temp: {0:.2f} C - {1:7.2f} hPa - Humidity: {2:.3f} %RH - Air quality: {3:.2f}'.format(
+            temperatur,
+            pressure,
+            hum,
+            air_quality_score))
+
+        Log.Msg('Temp: {0:.2f} C - {1:7.2f} hPa - Humidity: {2:.3f} %RH - Air quality: {3:.2f}'.format(
+            temperatur,
+            pressure,
+            hum,
+            air_quality_score))
+
+        DATA = 'Temp":{0:.2f} - "Pressure":{1:7.2f} - "Humidity":{2:.3f} - "AirQuality":{3:.2f}'.format(
+            temperatur,
+            pressure,
+            hum,
+            air_quality_score)
+
+        mqtt.send_data(DATA)
+
+        time.sleep(10)
+
+
+# Main program logic:
+if __name__ == '__main__':
+
+    burn_time()
+
+    mqtt = MqttHandler()
+
     while True:
-        if sensor.get_sensor_data() and sensor.data.heat_stable:
-            temperatur = sensor.data.temperature
-            pressure = sensor.data.pressure
+        main_air_quality(mqtt)
 
-            gas = sensor.data.gas_resistance
-            gas_offset = gas_baseline - gas
-
-            hum = sensor.data.humidity
-            hum_offset = hum - hum_baseline
-
-            # Calculate hum_score as the distance from the hum_baseline.
-            if hum_offset > 0:
-                hum_score = (100 - hum_baseline - hum_offset)
-                hum_score /= (100 - hum_baseline)
-                hum_score *= (hum_weighting * 100)
-
-            else:
-                hum_score = (hum_baseline + hum_offset)
-                hum_score /= hum_baseline
-                hum_score *= (hum_weighting * 100)
-
-            # Calculate gas_score as the distance from the gas_baseline.
-            if gas_offset > 0:
-                gas_score = (gas / gas_baseline)
-                gas_score *= (100 - (hum_weighting * 100))
-
-            else:
-                gas_score = 100 - (hum_weighting * 100)
-
-            # Calculate air_quality_score.
-            air_quality_score = hum_score + gas_score
-
-            # print('Temp: {0:.2f} C - {1:.2f} hPa - Humidity: {2:.3f} %RH - Gas: {3:.2f} Ohms - Air quality: {4:.2f}'.format(
-            #    temperatur,
-            #    pressure,
-            #    hum,
-            #    gas,
-            #    air_quality_score))
-
-            print('Temp: {0:.2f} C - {1:.2f} hPa - Humidity: {2:.3f} %RH - Air quality: {3:.2f}'.format(
-                temperatur,
-                pressure,
-                hum,
-                air_quality_score))
-
-            Log.Msg('Temp: {0:.2f} C - {1:.2f} hPa - Humidity: {2:.3f} %RH - Air quality: {3:.2f}'.format(
-                temperatur,
-                pressure,
-                hum,
-                air_quality_score))
-
-            DATA = 'Temp":{0:.2f} - "Pressure":{1:.2f} - "Humidity":{2:.3f} - "AirQuality":{3:.2f}'.format(
-                temperatur,
-                pressure,
-                hum,
-                air_quality_score)
-
-            publish_res = client.publish(TOPIC, DATA, qos=QOS)
-            if (not publish_res):
-
-                print('publish_res: ', publish_res)
-                Log.Msg('publish_res: ', publish_res)
-                client.connect(BROKER_ADDRESS, PORT)
-
-            time.sleep(5)
-
-except KeyboardInterrupt:
-    pass
+        mqtt.on_connected_new()
